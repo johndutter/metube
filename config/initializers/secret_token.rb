23836599720f6d687cfc31825c2563b451372fb2
Jassiem Moore <jassiemmoore@gmail.com>
2014-01-19 18:34:40 -0500

    Pushing secret token file.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Metube::Application.config.secret_key_base = '3633382bde2fd4dc1e88184915150f7d24efa2ff687acb7ba37f32d2c7016586a803146df202ba541978e409ab3a1ec6305d06f6c7fc9e43841b059e1fcb728b'
