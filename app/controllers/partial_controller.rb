9b4e659667bf7eb5bf4bb0999f183e75c4a9d62a
John Utter <johndutter@gmail.com>
2014-01-28 09:57:53 -0500

    moved check_login back

--- app/controllers/partial_controller.rb
+++ app/controllers/partial_controller.rb
@@ -10,15 +10,4 @@ class PartialController < ApplicationController
     render "secured/" + params[:partial] , :layout => false
   end
 
-  private
-  
-  #don't allow logged in user to access log in page
-  def check_login
-    if session[:user_id]
-      return true
-    else
-      head :unauthorized
-    end
-  end
-
 end
