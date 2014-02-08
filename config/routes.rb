54d689d191a61c2d3563cd40b750513ce404d0c9
John Utter <johndutter@gmail.com>
2014-02-08 16:48:35 -0500

    changed user pwd encryption

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Metube::Application.routes.draw do
   match "api/signup", to: "users#create", via: [:post]
   match "api/logout", to: "session#logout", via: [:post]
   match "api/get-user-info", to: "users#get_user_info", via: [:get]
+  match "api/get-user-profile", to: "users#get_user_profile", via: [:get]
+  match "api/update-user-profile", to: "users#update_user_profile", via: [:post]
+  match "api/update-user-password", to: "users#update_user_password", via: [:post]
+
   # for angular page prefetch
   match "api/loggedin", to: "session#loggedin", via: [:get]
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140123014608) do
+ActiveRecord::Schema.define(version: 20140207234928) do
 
   create_table "users", force: true do |t|
     t.string   "username"
@@ -21,6 +21,9 @@ ActiveRecord::Schema.define(version: 20140123014608) do
     t.datetime "updated_at"
     t.string   "email"
     t.decimal  "iterations",         precision: 10, scale: 0
+    t.string   "firstname"
+    t.string   "lastname"
+    t.string   "phone"
   end
 
 end
