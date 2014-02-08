e41d087252ae84d605d8343d8090f9da37456111
John Utter <johndutter@gmail.com>
2014-02-08 18:50:05 -0500

    ui for multimedia

--- app/assets/javascripts/controllers/multimedia_ctrl.js
+++ app/assets/javascripts/controllers/multimedia_ctrl.js
@@ -1,5 +1,19 @@
 // dashboard controller
-function MultimediaCtrl($scope) {
+function MultimediaCtrl($scope, $stateParams, apiService) {
+
+  $scope.init = function() {
+
+    // go get the multimedia type and information
+    apiService.apiCall(function(data, status) {
+      if (status === 200) {
+
+      } else {
+        
+      }
+    }, 'GET', '/api/get-multimedia-info', { id: $stateParams.id });
+
+  }
+  $scope.init();
 
 }
-MultimediaCtrl.$inject = ['$scope'];
+MultimediaCtrl.$inject = ['$scope', '$stateParams', 'apiService'];
--- app/assets/javascripts/services/api_service.js
+++ app/assets/javascripts/services/api_service.js
@@ -1,14 +1,23 @@
 // Service definition
 app.service('apiService', ['$http', function($http){
-    //args for apiCall (callback, requestMethod, action, requestData)
-    this.apiCall = function(callback){
-      $http({ method: arguments[1], url: arguments[2], timeout: 10000, data: arguments[3]}).
-          success(function(data, status, headers, config) {
-              callback(data, status);
-          }).
-          error(function(data, status, headers, config) {
-              callback(data, status);
-          });
-    };        
+
+  //args for apiCall (callback, requestMethod, action, requestData)
+  this.apiCall = function(callback){
+
+    var config = { method: arguments[1], url: arguments[2], timeout: 10000 };
+    if (arguments[1] === 'GET') {
+      _.extend(config, { params: arguments[3] });
+    } else {
+      _.extend(config, { data: arguments[3] });
+    }
+
+    $http(config).
+      success(function(data, status, headers, config) {
+          callback(data, status);
+      }).
+      error(function(data, status, headers, config) {
+          callback(data, status);
+      });
+  };        
 }]);
 
